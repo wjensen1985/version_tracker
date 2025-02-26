a11c98cc2479ce5ae23d7c1d57a7a0952b8d659b
wjensen1985 <william.hw.jensen@gmail.com>
2025-02-25 21:31:18 -0500

    added fetch project items

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,17 @@
-import { fetchUserProjects } from "../lib/data";
+import { fetchUserProjects, fetchProjectItems } from "../lib/data";
 
 export default async function Dashboard() {
-    const test_id = 1;
-    const projects = await fetchUserProjects(test_id.toString());
+    const test_user_id = 1;
+    const projects = await fetchUserProjects(test_user_id.toString());
+
+    const test_project_id = 2;
+    const items = await fetchProjectItems(test_project_id.toString());
 
     return (
       <main>
         <div>Hello Dashboard!</div>
         <div>{JSON.stringify(projects)}</div>
+        <div>{JSON.stringify(items)}</div>
       </main>
     );
   }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,6 +1,6 @@
 import { sql } from '@vercel/postgres';
 
-export async function fetchUserProjects(user: string) {
+export async function fetchUserProjects(user_id: string) {
   
     try {
       const projects = await sql`
@@ -10,7 +10,7 @@ export async function fetchUserProjects(user: string) {
             up.role
         FROM projects p
         JOIN user_projects up ON p.id = up.project_id
-        WHERE up.user_id = ${user};
+        WHERE up.user_id = ${user_id};
       `;
   
       return projects.rows;
@@ -18,4 +18,20 @@ export async function fetchUserProjects(user: string) {
       console.error('Database Error:', error);
       throw new Error('Failed to fetch projects.');
     }
+}
+
+export async function  fetchProjectItems(project_id: string) {
+    try {
+        const items = await sql`
+          SELECT *
+          FROM items
+          JOIN projects p ON items.project_id = p.id
+          WHERE p.id = ${project_id};
+        `;
+    
+        return items.rows;
+      } catch (error) {
+        console.error('Database Error:', error);
+        throw new Error('Failed to fetch project items.');
+      }
 }
